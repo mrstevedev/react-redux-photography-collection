a6eac0b7044dbf8e96f9f36371047cfa31d3416c
Steven Polido <steven.polido@vistage.com>
2019-12-16 17:00:40 -0800

    Added named anchors.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,18 +11,18 @@ class Sidebar extends Component {
                  <nav className="vertical" data-aos="fade-right">
                     <ul className="photo-list">
                     <h4 className="photo-list-header">My Photos <i className="far fa-images"></i></h4>
-                        <li className={this.props.active ? 'active' : ''}><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Misty Morning</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Early Morning</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Sun</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Reflection</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Trolly</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Bridge</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Wonder</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Bonita Thoughts</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>DJ Tech Set</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>DJ Tech Set II</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>DJ Tech Set III</a></li>
-                        <li><a className="nav-link" href="#" onClick={this.props.handleClick}>Loft Couple</a></li>
+                        <li className={this.props.active ? 'active' : ''}><a className="nav-link" href="#one" onClick={this.props.handleClick}>Bonita Misty Morning</a></li>
+                        <li><a className="nav-link" href="#two" onClick={this.props.handleClick}>Bonita Early Morning</a></li>
+                        <li><a className="nav-link" href="#three" onClick={this.props.handleClick}>Bonita Sun</a></li>
+                        <li><a className="nav-link" href="#four" onClick={this.props.handleClick}>Bonita Reflection</a></li>
+                        <li><a className="nav-link" href="#five" onClick={this.props.handleClick}>Bonita Trolly</a></li>
+                        <li><a className="nav-link" href="#six" onClick={this.props.handleClick}>Bonita Bridge</a></li>
+                        <li><a className="nav-link" href="#seven" onClick={this.props.handleClick}>Bonita Wonder</a></li>
+                        <li><a className="nav-link" href="#eight" onClick={this.props.handleClick}>Bonita Thoughts</a></li>
+                        <li><a className="nav-link" href="#nine" onClick={this.props.handleClick}>DJ Tech Set</a></li>
+                        <li><a className="nav-link" href="#ten" onClick={this.props.handleClick}>DJ Tech Set II</a></li>
+                        <li><a className="nav-link" href="#eleven" onClick={this.props.handleClick}>DJ Tech Set III</a></li>
+                        <li><a className="nav-link" href="#twelve" onClick={this.props.handleClick}>Loft Couple</a></li>
                     </ul>
                     <ul className="info-toggle-list">
                         <li><a href=""><i className="fas fa-info-circle"></i></a></li>
