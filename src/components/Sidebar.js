db5c0f8615849451f341eeaaa3f35fda4b1d058e
Steven Polido <steven.polido@vistage.com>
2020-01-03 20:49:45 -0800

    Added event handlers to navigation links.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,15 +16,15 @@ class Sidebar extends Component {
                         <li className={this.props.active === 'one' ? 'active' : ''}><a className="nav-link" href="#one" onClick={(e) => this.props.handleClick(e, 'one')}>Bonita Misty Morning</a></li>
                         <li className={this.props.active === 'two' ? 'active' : ''}><a className="nav-link" href="#two" onClick={(e) => this.props.handleClick(e, 'two')}>Bonita Early Morning</a></li>
                         <li className={this.props.active === 'three' ? 'active' : ''}><a className="nav-link" href="#three" onClick={(e) => this.props.handleClick(e, 'three')}>Bonita Sun</a></li>
-                        <li className={this.props.active === 'four' ? 'active' : ''}><a className="nav-link" href="#four" onClick={this.props.handleClick}>Bonita Reflection</a></li>
-                        <li className={this.props.active === 'five' ? 'active' : ''}><a className="nav-link" href="#five" onClick={this.props.handleClick}>Bonita Trolly</a></li>
-                        <li className={this.props.active === 'six' ? 'active' : ''}><a className="nav-link" href="#six" onClick={this.props.handleClick}>Bonita Bridge</a></li>
-                        <li className={this.props.active === 'seven' ? 'active' : ''}><a className="nav-link" href="#seven" onClick={this.props.handleClick}>Bonita Wonder</a></li>
-                        <li className={this.props.active === 'eight' ? 'active' : ''}><a className="nav-link" href="#eight" onClick={this.props.handleClick}>Bonita Thoughts</a></li>
-                        <li className={this.props.active === 'nine' ? 'active' : ''}><a className="nav-link" href="#nine" onClick={this.props.handleClick}>DJ Tech Set</a></li>
-                        <li className={this.props.active === 'ten' ? 'active' : ''}><a className="nav-link" href="#ten" onClick={this.props.handleClick}>DJ Tech Set II</a></li>
-                        <li className={this.props.active === 'eleven' ? 'active' : ''}><a className="nav-link" href="#eleven" onClick={this.props.handleClick}>DJ Tech Set III</a></li>
-                        <li className={this.props.active === 'twelve' ? 'active' : ''}><a className="nav-link" href="#twelve" onClick={this.props.handleClick}>Loft Couple</a></li>
+                        <li className={this.props.active === 'four' ? 'active' : ''}><a className="nav-link" href="#four" onClick={(e) => this.props.handleClick(e, 'four')}>Bonita Reflection</a></li>
+                        <li className={this.props.active === 'five' ? 'active' : ''}><a className="nav-link" href="#five" onClick={(e) => this.props.handleClick(e, 'five')}>Bonita Trolly</a></li>
+                        <li className={this.props.active === 'six' ? 'active' : ''}><a className="nav-link" href="#six" onClick={(e) => this.props.handleClick(e, 'six')}>Bonita Bridge</a></li>
+                        <li className={this.props.active === 'seven' ? 'active' : ''}><a className="nav-link" href="#seven" onClick={(e) => this.props.handleClick(e, 'seven')}>Bonita Wonder</a></li>
+                        <li className={this.props.active === 'eight' ? 'active' : ''}><a className="nav-link" href="#eight" onClick={(e) => this.props.handleClick(e, 'eight')}>Bonita Thoughts</a></li>
+                        <li className={this.props.active === 'nine' ? 'active' : ''}><a className="nav-link" href="#nine" onClick={(e) => this.props.handleClick(e, 'nine')}>DJ Tech Set</a></li>
+                        <li className={this.props.active === 'ten' ? 'active' : ''}><a className="nav-link" href="#ten" onClick={(e) => this.props.handleClick(e, 'ten')}>DJ Tech Set II</a></li>
+                        <li className={this.props.active === 'eleven' ? 'active' : ''}><a className="nav-link" href="#eleven" onClick={(e) => this.props.handleClick(e, 'eleven')}>DJ Tech Set III</a></li>
+                        <li className={this.props.active === 'twelve' ? 'active' : ''}><a className="nav-link" href="#twelve" onClick={(e) => this.props.handleClick(e, 'twelve')}>Loft Couple</a></li>
                     </ul>
                     <ul className="info-toggle-list">
                         <li><a href="" onClick={(e) => this.props.handleSideBarClick(e, 'Camera Info')}><i className="fas fa-info-circle"></i></a></li>
