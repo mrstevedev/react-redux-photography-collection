53afa4051195c1c62cb404286abb9870dd79b556
Steven Polido <steven.polido@vistage.com>
2020-01-24 13:55:50 -0800

    Updated nav.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => (
         <nav className="nav-right horizontal" data-aos="fade" data-aos-delay="800">
             <ul>
                <li><Link className="nav-link" to="/">Home</Link></li>
-               <li><Link className="nav-link" to="/about">Archives</Link></li>
+               <li><Link className="nav-link" to="/about">About</Link></li>
                <li><Link className="nav-link disabled" to="#">Speaking</Link></li>
                <li><Link className="nav-link disabled" to="#">RSS Feed</Link></li>
            </ul>
