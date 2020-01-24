170974f65391e720089fddc150f0678b7c98447e
Steven Polido <steven.polido@vistage.com>
2020-01-24 12:30:59 -0800

    Updated BackToTop Component.

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 
-class BackToTop extends Component {  
-  render() {
+const BackToTop = (props) => {  
     return (
       <div>
         <div className="back-to-top" data-aos="fade-up" data-aos-delay="1000" data-aos-offset="800">
-          <a href="#!" className="back-to-top-btn" onClick={this.props.basicScrollTop}>
+          <a className="back-to-top-btn" onClick={topScrollTo}>
             <i className="fas fa-long-arrow-alt-up"></i>Back To Top
           </a>
         </div>
       </div>
     );
-  }
 }
+
+
+   // Scroll to top
+   // Smooth scroll top
+   let topScrollTo = function() {
+     if (window.scrollY != 0) {
+       setTimeout(function() {
+         window.scrollTo(0, window.scrollY - 30);
+         topScrollTo();
+       }, 5);
+     }
+   };
+
 
 export default BackToTop;
