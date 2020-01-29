656b5ff8f0d885376ae69ffbff382f2805fa92c4
Steven Polido <steven.polido@vistage.com>
2020-01-28 16:01:43 -0800

    Added contact form logic.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,45 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import Logo from "./Logo";
+import axios from 'axios';
 
 const Contact = props => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Contact form submitted");
+    console.log(`Submitting name ${name}`);
+    console.log(`Submitting email ${email}`);
+    console.log(`Submitting message ${message}`);
+    axios({
+      method: 'POST',
+      url: 'http://localhost:4000/success',
+      data: {
+        name,
+        email,
+        message
+      }
+    }).then((res) => {
+      if(res.data === 'success') {
+        console.log('Message Sent');
+        setSuccess('Success');
+        resetForm();
+      } else if(res.data === 'fail') {
+        console.log('Message Fail');
+      }
+    }).catch(err => console.log(err))
+  };
+
+  const resetForm = () => {
+    document.getElementById('contact-form').reset();
+  }
+
   const handleLink = () => {
     props.history.push("/");
   };
@@ -13,7 +49,6 @@ const Contact = props => {
         className="contact"
         data-aos="fade"
         data-aos-delay="100"
-        onSubmit={handleSubmit}
       >
         <div className="left"></div>
         <div className="right">
@@ -23,30 +58,43 @@ const Contact = props => {
                 <i className="fas fa-angle-double-left"></i>
               </Link>
             </div>
-            <div className="logo">
+            <div className="logo-container">
               <Logo />
             </div>
           </header>
 
           <div className="contact-form">
-            <form action="/success" method="POST">
-              <label>
-                <input type="text" name="name" required placeholder="Name" />
-              </label>
+           {!success ? (
+ <form id="contact-form" onSubmit={handleSubmit} method="POST">
+ <label>
+   <input 
+     type="text" 
+     name="name" 
+     required 
+     placeholder="Name" 
+     onChange={event => setName(event.target.value)} />
+ </label>
 
-              <label>
-                <input type="text" name="email" required placeholder="Email" />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  name="message"
-                  required
-                  placeholder="Message"
-                />
-              </label>
-              <input type="submit" value="Submit" />
-            </form>
+ <label>
+   <input type="text" 
+     name="email" 
+     required 
+     placeholder="Email" 
+     onChange={event => setEmail(event.target.value)}  
+     />
+ </label>
+ <label>
+   <input
+     type="text"
+     name="message"
+     required
+     placeholder="Message"
+     onChange={event => setMessage(event.target.value)}  
+   />
+ </label>
+ <input type="submit" value="Submit" />
+</form>
+           ) : 'Message Sent'}
           </div>
         </div>
       </section>
@@ -54,9 +102,6 @@ const Contact = props => {
   );
 };
 
-const handleSubmit = e => {
-  console.log("Contact form submitted");
-  // e.preventDefault();
-};
+
 
 export default Contact;
