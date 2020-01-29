d917f7b5f184fdd23ab0fd253bf8dd6f63d5beee
Steven Polido <steven.polido@vistage.com>
2020-01-28 16:21:01 -0800

    Updated About component with history and additional layout changes.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from './Header';
+import { Link } from "react-router-dom";
 
-const About = () => {
+const About = (props) => {
+    const handleLink = () => {
+        props.history.push("/");    
+    }
     return (
+        <Fragment>
+            <Header />
         <div className="container">
-            <img src="/img/logo.svg" className="logo" />
-            <h4 className="logo-sub-txt">Photography</h4>
+            {/* <Link className="goback" to="#!" onClick={handleLink}>
+            <i className="fas fa-angle-double-left"></i>
+            </Link> */}
+            <div class="demo image above"> 
+                <section class="vert">
+                    <header></header>
+                    <div class="separator"></div>
+                    <footer></footer>
+                </section>
+                <section className="about">            
+                <p className="about-text">
+                    Curabitur aliquet quam id dui posuere blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, 
+                    auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. 
+                    Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada. Sed porttitor lectus nibh. 
+                    Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada.
+                </p>
+            </section>
+            </div>        
         </div>
+        </Fragment>
     )
 }
 
