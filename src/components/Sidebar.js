0ba80113693a115a355389da0a9372a2acbfd8a8
Steven Polido <steven.polido@vistage.com>
2020-01-29 12:00:51 -0800

    Updated nav to aside.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,45 +17,53 @@ const Sidebar = (props) => {
         if(active){
           active.classList.remove('active');
         }
-        e.currentTarget.parentNode.classList.add('active');
+        e.currentTarget.querySelector('.nav-link li').classList.add('active');
         store.dispatch(setInfo(val))
       }
         return (
             <Fragment>
-                 <nav className="vertical" data-aos="fade-right">
+                 <aside className="vertical" data-aos="fade-right">
                     <ul className="photo-list">
                     <h4 className="photo-list-header">My Photos <i className="far fa-images"></i></h4>
-                        <li className={props.active === 'one' ? 'active' : ''}><a className="nav-link" href="#one" onClick={(e) => dispatchAction(e, 'one')}>Bonita Misty Morning</a></li>
-                        <li className={props.active === 'two' ? 'active' : ''}><a className="nav-link" href="#two" onClick={(e) => dispatchAction(e, 'two')}>Bonita Early Morning</a></li>
-                        <li className={props.active === 'three' ? 'active' : ''}><a className="nav-link" href="#three" onClick={(e) => dispatchAction(e, 'three')}>Bonita Sun</a></li>
-                        <li className={props.active === 'four' ? 'active' : ''}><a className="nav-link" href="#four" onClick={(e) => dispatchAction(e, 'four')}>Bonita Reflection</a></li>
-                        <li className={props.active === 'five' ? 'active' : ''}><a className="nav-link" href="#five" onClick={(e) => dispatchAction(e, 'five')}>Bonita Trolly</a></li>
-                        <li className={props.active === 'six' ? 'active' : ''}><a className="nav-link" href="#six" onClick={(e) => dispatchAction(e, 'six')}>Bonita Bridge</a></li>
-                        <li className={props.active === 'seven' ? 'active' : ''}><a className="nav-link" href="#seven" onClick={(e) => dispatchAction(e, 'seven')}>Bonita Wonder</a></li>
-                        <li className={props.active === 'eight' ? 'active' : ''}><a className="nav-link" href="#eight" onClick={(e) => dispatchAction(e, 'eight')}>Bonita Thoughts</a></li>
-                        <li className={props.active === 'nine' ? 'active' : ''}><a className="nav-link" href="#nine" onClick={(e) => dispatchAction(e, 'nine')}>DJ Tech Set</a></li>
-                        <li className={props.active === 'ten' ? 'active' : ''}><a className="nav-link" href="#ten" onClick={(e) => dispatchAction(e, 'ten')}>DJ Tech Set II</a></li>
-                        <li className={props.active === 'eleven' ? 'active' : ''}><a className="nav-link" href="#eleven" onClick={(e) => dispatchAction(e, 'eleven')}>DJ Tech Set III</a></li>
-                        <li className={props.active === 'twelve' ? 'active' : ''}><a className="nav-link" href="#twelve" onClick={(e) => dispatchAction(e, 'twelve')}>Loft Couple</a></li>
+                        <a className="nav-link" href="#one" onClick={(e) => dispatchAction(e, 'one')}><li className={props.active === 'one' ? 'active' : ''}>Bonita Misty Morning</li></a>
+                        <a className="nav-link" href="#two" onClick={(e) => dispatchAction(e, 'two')}><li className={props.active === 'two' ? 'active' : ''}>Bonita Early Morning</li></a>
+                        <a className="nav-link" href="#three" onClick={(e) => dispatchAction(e, 'three')}><li className={props.active === 'three' ? 'active' : ''}>Bonita Sun</li></a>
+                        <a className="nav-link" href="#four" onClick={(e) => dispatchAction(e, 'four')}><li className={props.active === 'four' ? 'active' : ''}>Bonita Reflection</li></a>
+                        <a className="nav-link" href="#five" onClick={(e) => dispatchAction(e, 'five')}><li className={props.active === 'five' ? 'active' : ''}>Bonita Trolly</li></a>
+                        <a className="nav-link" href="#six" onClick={(e) => dispatchAction(e, 'six')}><li className={props.active === 'six' ? 'active' : ''}>Bonita Bridge</li></a>
+                        <a className="nav-link" href="#seven" onClick={(e) => dispatchAction(e, 'seven')}><li className={props.active === 'seven' ? 'active' : ''}>Bonita Wonder</li></a>
+                        <a className="nav-link" href="#eight" onClick={(e) => dispatchAction(e, 'eight')}><li className={props.active === 'eight' ? 'active' : ''}>Bonita Thoughts</li></a>
+                        <a className="nav-link" href="#nine" onClick={(e) => dispatchAction(e, 'nine')}><li className={props.active === 'nine' ? 'active' : ''}>DJ Tech Set</li></a>
+                        <a className="nav-link" href="#ten" onClick={(e) => dispatchAction(e, 'ten')}><li className={props.active === 'ten' ? 'active' : ''}>DJ Tech Set II</li></a>
+                        <a className="nav-link" href="#eleven" onClick={(e) => dispatchAction(e, 'eleven')}><li className={props.active === 'eleven' ? 'active' : ''}>DJ Tech Set III</li></a>
+                        <a className="nav-link" href="#twelve" onClick={(e) => dispatchAction(e, 'twelve')}> <li className={props.active === 'twelve' ? 'active' : ''}>Loft Couple</li></a>
                     </ul>
                     <ul className="info-toggle-list">
                         {/* <li><a href="#!" onClick={(e) => props.handleSideBarClick(e, 'Camera Info')}><i className="fas fa-info-circle"></i></a></li> */}
                         <CameraInfo 
-                            handleCameraInfo={(e) => props.handleSideBarClick(e, 'Camera Info')} />
+                          handleCameraInfo={(e) => props.handleSideBarClick(e, 'Camera Info')} />
 
                         <CommentPhoto
-                            handleComment={(e) => props.handleSideBarClick(e, 'Comments')} />
+                          handleComment={(e) => props.handleSideBarClick(e, 'Comments')} />
 
                         <SharePhoto 
-                            handleShare={(e) => props.handleSideBarClick(e, 'Share')} 
+                          handleShare={(e) => props.handleSideBarClick(e, 'Share')} 
                         />
                         <DownloadPhoto
-                            imageName={store.getState().imageName}
-                            imagePath={store.getState().imagePath}
-                            handlePhotoDownload={(e) => dispatchAction(e, 'Download_Photo')}
+                          imageName={store.getState().imageName}
+                          imagePath={store.getState().imagePath}
+                          handlePhotoDownload={(e) => dispatchAction(e, 'Download_Photo')}
                         />
                     </ul>
-                    </nav>
+                    <ul className="copyright-list">
+                      <li>
+                        <p className="copyright">Steven Pulido | &copy; 2020</p>                        
+                        </li>
+                        <li>
+                        <p className="copyright">Built with React & Redux</p>
+                      </li>
+                    </ul>
+                    </aside>
             </Fragment>
         ); 
 }
