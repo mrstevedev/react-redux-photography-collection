754784fc541d82d1621f9c0e504344a482ed67ad
Steven Polido <steven.polido@vistage.com>
2020-01-28 16:18:24 -0800

    Removed extra link.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => (
     <Fragment>
-        <nav className="nav-right horizontal" data-aos="fade" data-aos-delay="800">
+        <nav className="nav-right horizontal">
             <ul>
                <li><Link className="nav-link" to="/">Home</Link></li>
                <li><Link className="nav-link" to="/about">About</Link></li>
-               <li><Link className="nav-link disabled" to="#">Speaking</Link></li>
-               <li><Link className="nav-link disabled" to="#">RSS Feed</Link></li>
+               {/* <li><Link className="nav-link disabled" to="#">Speaking</Link></li> */}
+               <li><Link className="nav-link disabled" to="/contact">Contact</Link></li>
            </ul>
           </nav>
     </Fragment>
