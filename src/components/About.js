24cc6812ec65deb964d423883c63fdff31c71bef
Steven Polido <steven.polido@vistage.com>
2020-01-29 12:00:29 -0800

    Updated go back link.

--- src/components/About.js
+++ src/components/About.js
@@ -9,17 +9,17 @@ const About = (props) => {
     return (
         <Fragment>
             <Header />
-        <div className="container">
-            {/* <Link className="goback" to="#!" onClick={handleLink}>
-            <i className="fas fa-angle-double-left"></i>
-            </Link> */}
+        <div className="container">           
             <div class="demo image above"> 
                 <section class="vert">
                     <header></header>
                     <div class="separator"></div>
                     <footer></footer>
                 </section>
-                <section className="about">            
+                <section className="about"> 
+                <Link className="goback" to="#!" onClick={handleLink}>
+            <i className="fas fa-angle-double-left"></i>
+            </Link>           
                 <p className="about-text">
                     Curabitur aliquet quam id dui posuere blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, 
                     auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. 
