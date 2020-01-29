06e325aff1c9db44bc5688302064e88c3f29220c
Steven Polido <steven.polido@vistage.com>
2020-01-28 16:17:15 -0800

    Updated reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 export default (state, action) => {
   console.log(state, action);
   switch (action.type) {
-    case "SET_INFO":
-      return {
-        ...state,
-        photo: action.photo
-      };
+      case "SET_PHOTO": 
+        return {
+          ...state,
+          photo: action.photo
+        }
       default:
           return state;
   }
