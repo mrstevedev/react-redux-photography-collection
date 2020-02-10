3d6aaba7d3845b88e394787b979d164da5171ed1
Steven Polido <steven.polido@vistage.com>
2020-02-10 11:05:52 -0800

    Updated CameraInfo Component.

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -3,11 +3,11 @@ import React, { Fragment } from "react";
 const CameraInfo = (props) => {
   return (
     <Fragment>
-      <li>
         <a href="#!" onClick={e => props.handleCameraInfo(e, "Camera Info")}>
-          <i className="fas fa-comments"></i>
-        </a>
+      <li>
+          <i className="fas fa-info-circle"></i>
       </li>
+        </a>
     </Fragment>
   );
 };
