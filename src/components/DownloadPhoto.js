b8b697677bd385087d6e31880016587f5916dddb
Steven Polido <steven.polido@vistage.com>
2020-02-10 11:22:34 -0800

    Updated DownloadPhoto Component.

--- src/components/DownloadPhoto.js
+++ src/components/DownloadPhoto.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-const DownloadPhotoIcon = (props) => {
-    return (
-        <Fragment>
-            <li>
-                <a 
-                href={props.imagePath} 
-                download={props.imageName} 
-                onClick={(e) => props.handlePhotoDownload(e)}>
-                <i className="fas fa-download"></i>
-                </a>
-            </li>
-        </Fragment>
-    );
-    
-}
-export default DownloadPhotoIcon;
+const DownloadPhoto = props => {
+  return (
+    <Fragment>
+      <a
+        className="nav-link"
+        href={props.imagePath}
+        download={props.imageName}
+        onClick={e => props.handlePhotoDownload(e)}>
+        <li>
+          <i className="fas fa-download"></i>
+        </li>
+      </a>
+    </Fragment>
+  );
+};
+export default DownloadPhoto;
