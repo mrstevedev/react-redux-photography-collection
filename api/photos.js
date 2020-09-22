fc52584e76aac7334d8c1f5baabd36fb17d7328b
Steven Polido <steven.polido@vistage.com>
2020-09-22 16:18:24 -0700

    Added api/ folder and api schema

---
+++ api/photos.js
@@ -0,0 +1,76 @@
+const photos = [
+    {
+        id: 1,
+        photo_1: "Bonita Misty Morning",
+        location: 'Eastlake, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 2,
+        photo_1: "Bonita Early Morning",
+        location: 'Eastlake, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 3,
+        photo_1: "Bonita Sun",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 4,
+        photo_1: "Bonita Reflection",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 5,
+        photo_1: "Bonita Trolly",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 6,
+        photo_1: "Bonita Bridge",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 7,
+        photo_1: "Bonita Wonder",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 8,
+        photo_1: "Bonita Thoughts",
+        location: 'Logan, CA',
+        camera: 'Nikon D3300'
+    },
+    {
+        id: 9,
+        photo_1: "DJ Tech Set I",
+        location: 'Logan, CA',
+        camera: 'GS Raptor Smartphone'
+    },
+    {
+        id: 10,
+        photo_1: "DJ Tech Set II",
+        location: 'Logan, CA',
+        camera: 'GS Raptor Smartphone'
+    },
+    {
+        id: 11,
+        photo_1: "DJ Tech Set III",
+        location: 'Logan, CA',
+        camera: 'GS Raptor Smartphone'
+    },
+    {
+        id: 12,
+        photo_1: "Loft Couple",
+        location: 'Logan, CA',
+        camera: 'GS Raptor Smartphone'
+    }, 
+]
+
+module.exports = photos;
