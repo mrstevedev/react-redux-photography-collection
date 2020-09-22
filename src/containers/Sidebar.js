4423ddd94f060cbff603b9a6a3a200cab581dc65
Steven Polido <steven.polido@vistage.com>
2020-09-22 14:44:17 -0700

    Removed font awesome icon

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -24,7 +24,7 @@ const Sidebar = (props) => {
             <Fragment>
                  <aside className="vertical" data-aos="fade-right">
                     <ul className="photo-list">
-                    <h4 className="photo-list-header">My Photos <i className="far fa-images"></i></h4>
+                    <h4 className="photo-list-header">My Photos </h4>
                         <a className="nav-link" href="#one" onClick={(e) => handleSideBarClick(e, 'one')}><li className={props.active === 'one' ? 'active' : ''}>Bonita Misty Morning</li></a>
                         <a className="nav-link" href="#two" onClick={(e) => handleSideBarClick(e, 'two')}><li className={props.active === 'two' ? 'active' : ''}>Bonita Early Morning</li></a>
                         <a className="nav-link" href="#three" onClick={(e) => handleSideBarClick(e, 'three')}><li className={props.active === 'three' ? 'active' : ''}>Bonita Sun</li></a>
@@ -38,23 +38,23 @@ const Sidebar = (props) => {
                         <a className="nav-link" href="#eleven" onClick={(e) => handleSideBarClick(e, 'eleven')}><li className={props.active === 'eleven' ? 'active' : ''}>DJ Tech Set III</li></a>
                         <a className="nav-link" href="#twelve" onClick={(e) => handleSideBarClick(e, 'twelve')}> <li className={props.active === 'twelve' ? 'active' : ''}>Loft Couple</li></a>
                     </ul>
-                    <ul className="info-toggle-list">
+                    {/* <ul className="info-toggle-list">
                         {/* <li><a href="#!" onClick={(e) => props.handleSideBarClick(e, 'Camera Info')}><i className="fas fa-info-circle"></i></a></li> */}
-                        <CameraInfo 
-                          handleCameraInfo={(e) => props.handleSideBarClick(e, 'Camera Info')} />
+                        {/* <CameraInfo 
+                          handleCameraInfo={(e) => props.handleSideBarClick(e, 'Camera Info')} /> */}
 
-                        <CommentPhoto
-                          handleComment={(e) => props.handleSideBarClick(e, 'Comments')} />
-
-                        <SharePhoto 
+                        {/* <CommentPhoto
+                          handleComment={(e) => props.handleSideBarClick(e, 'Comments')} /> */}
+                      
+                      {/*  <SharePhoto 
                           handleShare={(e) => props.handleSideBarClick(e, 'Share')} 
                         />
                         <DownloadPhoto
                           imageName={store.getState().imageName}
                           imagePath={store.getState().imagePath}
                           handlePhotoDownload={(e) => dispatchAction(e, 'Download_Photo')}
-                        />
-                    </ul>
+                        /> */}
+                    {/* </ul> */}
                     <ul className="copyright-list">
                       <li>
                         <p className="copyright">Steven Pulido | &copy; 2020</p>                        
