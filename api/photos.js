87dc4d1b00ea80f848cda293f455bb188e0b97ad
Steven Polido <steven.polido@vistage.com>
2020-09-23 15:54:41 -0700

    Updated property name to title

--- api/photos.js
+++ api/photos.js
@@ -1,73 +1,73 @@
 const photos = [
     {
         id: 1,
-        photo_1: "Bonita Misty Morning",
+        title: "Bonita Misty Morning",
         location: 'Eastlake, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 2,
-        photo_1: "Bonita Early Morning",
+        title: "Bonita Early Morning",
         location: 'Eastlake, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 3,
-        photo_1: "Bonita Sun",
+        title: "Bonita Sun",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 4,
-        photo_1: "Bonita Reflection",
+        title: "Bonita Reflection",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 5,
-        photo_1: "Bonita Trolly",
+        title: "Bonita Trolly",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 6,
-        photo_1: "Bonita Bridge",
+        title: "Bonita Bridge",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 7,
-        photo_1: "Bonita Wonder",
+        title: "Bonita Wonder",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 8,
-        photo_1: "Bonita Thoughts",
+        title: "Bonita Thoughts",
         location: 'Logan, CA',
         camera: 'Nikon D3300'
     },
     {
         id: 9,
-        photo_1: "DJ Tech Set I",
+        title: "DJ Tech Set I",
         location: 'Logan, CA',
         camera: 'GS Raptor Smartphone'
     },
     {
         id: 10,
-        photo_1: "DJ Tech Set II",
+        title: "DJ Tech Set II",
         location: 'Logan, CA',
         camera: 'GS Raptor Smartphone'
     },
     {
         id: 11,
-        photo_1: "DJ Tech Set III",
+        title: "DJ Tech Set III",
         location: 'Logan, CA',
         camera: 'GS Raptor Smartphone'
     },
     {
         id: 12,
-        photo_1: "Loft Couple",
+        title: "Loft Couple",
         location: 'Logan, CA',
         camera: 'GS Raptor Smartphone'
     }, 
