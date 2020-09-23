3005c6da0ed4ca607798521247776041d1212074
Steven Polido <steven.polido@vistage.com>
2020-09-23 15:27:11 -0700

    Updated logo img path

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Fragment>
       <div>
-        <img src="/img/logo.svg" className="logo" />
+        <img src="./img/logo.svg" className="logo" />
         <h4 className="logo-sub-txt">Photography</h4>
       </div>
     </Fragment>
