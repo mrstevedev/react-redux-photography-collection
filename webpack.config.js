aed7d20470086d325216f4fe9f28b4cae204f17e
Steven Polido <steven.polido@vistage.com>
2020-09-23 15:26:49 -0700

    Added collapseWhitespace in HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
     }),
     new CopyWebpackPlugin([
       {from: './public/'}
