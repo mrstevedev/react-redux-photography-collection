26a9b954f36bc28209da56ec9dff4e1d68119817
Steven Polido <steven.polido@vistage.com>
2020-09-23 22:39:29 -0700

    Updated sideBarRight component to receive props

--- src/components/SideBarRight.js
+++ src/components/SideBarRight.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import store from '../store';
 
 const SideBarRight = (props) => {
+  const cameraInfo = JSON.parse(localStorage.getItem('cameraInfoContent'));
     return (
       <aside className={`vertical right ` + (props.showSideBarRight === true ? 'show' : '' || localStorage.getItem('sidebarRight') !== null ? 'show' : '')}>
         <div className="camera-info-close"><i className="fas fa-times" onClick={props.handleClose}></i></div>
@@ -10,16 +11,22 @@ const SideBarRight = (props) => {
             {localStorage.getItem('sidebarRight') ? <h3><i className="fas fa-info-circle"></i>{localStorage.getItem('sidebarRight')}</h3> : null}
           </div>
           <div className="camera-info-info-body">
-            {props.title === 'Camera Info' || localStorage.getItem('cameraInfoContent') !== null ? (
-                  <div dangerouslySetInnerHTML={{__html: JSON.parse(localStorage.getItem('cameraInfoContent')) }}/>
-              ) : (
-                  props.title === 'Comments' || localStorage.getItem('commentInfoContent') !== null ?  (
-                    <div dangerouslySetInnerHTML={{__html: JSON.parse(localStorage.getItem('commentInfoContent')) }}/>
-              )
-                : (
-                  props.title === 'Share' || localStorage.getItem('shareInfoContent') !== null ? (
-                    <div dangerouslySetInnerHTML={{__html: JSON.parse(localStorage.getItem('shareInfoContent')) }} />
-              ) : null ))}
+              <table style={{ width: '100%' }}>
+                <tbody>                    
+                <tr>
+                    <th>Title</th>
+                    <td>{ props.currentPhoto.title ? props.currentPhoto.title : cameraInfo.title }</td>
+                </tr>
+                <tr>
+                    <th>Location</th>
+                    <td>{ props.currentPhoto.location ? props.currentPhoto.location : cameraInfo.location }</td>
+                </tr>
+                <tr>
+                    <th>Camera</th>
+                    <td>{ props.currentPhoto.camera ? props.currentPhoto.camera : cameraInfo.camera }</td>
+                </tr>
+                </tbody>     
+              </table>
           </div>
         </div>
       </aside>
