e35ab069d241f15e02d0e12e399dc76df83c50b0
Steven Polido <steven.polido@vistage.com>
2020-09-23 20:48:11 -0700

    Added CameraInfo component and added props to image src

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
+import CameraInfo from './CameraInfo';
 
-const Photo = ({ imageName, imagePath }) => {
+const Photo = (props) => {
   return (
     <Fragment>
-        <section data-aos="fade" data-aos-delay="30" id="one">
-            <img src={ imagePath } />
+        <section data-aos="fade" data-aos-delay="30">
+          <CameraInfo handleRightSideBarClick={props.handleRightSideBarClick} currPhoto={props.currentPhoto} />
+            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : './img/bonitaskies.jpg' } />
         </section>
     </Fragment>
   );
