3bf3fdae7486a5292e6094851ba3cb4d7b211de9
Steven Polido <steven.polido@vistage.com>
2020-09-23 21:11:30 -0700

    Added imagePath property

--- api/photos.js
+++ api/photos.js
@@ -1,75 +1,99 @@
 const photos = [
     {
         id: 1,
+        href: "one",
         title: "Bonita Misty Morning",
         location: 'Eastlake, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitaskies.jpg'
     },
     {
         id: 2,
+        href: "two",
         title: "Bonita Early Morning",
         location: 'Eastlake, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitaskies2.jpg'
     },
     {
         id: 3,
+        href: "three",
         title: "Bonita Sun",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitasun.jpg'
     },
     {
         id: 4,
+        href: "four",
         title: "Bonita Reflection",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitasunreflection.jpg'
     },
     {
         id: 5,
+        href: "five",
         title: "Bonita Trolly",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitatrolly.jpg'
     },
     {
         id: 6,
+        href: "six",
         title: "Bonita Bridge",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitabridge1.jpg'
     },
     {
         id: 7,
+        href: "seven",
         title: "Bonita Wonder",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitabridge.jpg'
     },
     {
         id: 8,
+        href: "eight",
         title: "Bonita Thoughts",
         location: 'Logan, CA',
-        camera: 'Nikon D3300'
+        camera: 'Nikon D3300',
+        imagePath: './img/bonitaclose.jpg'
     },
     {
         id: 9,
+        href: "nine",
         title: "DJ Tech Set I",
         location: 'Logan, CA',
-        camera: 'GS Raptor Smartphone'
+        camera: 'GS Raptor Smartphone',
+        imagePath: './img/djtech.jpg'
     },
     {
         id: 10,
+        href: "ten",
         title: "DJ Tech Set II",
         location: 'Logan, CA',
-        camera: 'GS Raptor Smartphone'
+        camera: 'GS Raptor Smartphone',
+        imagePath: './img/djtech2.jpg'
     },
     {
         id: 11,
+        href: "eleven",
         title: "DJ Tech Set III",
         location: 'Logan, CA',
-        camera: 'GS Raptor Smartphone'
+        camera: 'GS Raptor Smartphone',
+        imagePath: './img/djtech3.jpg'
     },
     {
         id: 12,
+        href: "twelve",
         title: "Loft Couple",
         location: 'Logan, CA',
-        camera: 'GS Raptor Smartphone'
+        camera: 'GS Raptor Smartphone',
+        imagePath: './img/loftcouple.jpg'
     }, 
 ]
 
