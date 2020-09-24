d316fada6ed7328f49f4b823500efee568c653fa
Steven Polido <steven.polido@vistage.com>
2020-09-23 20:50:16 -0700

    Added current photo to function argument and renamed function handler name

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 
 const CameraInfo = (props) => {
+  console.log('props:::::',props)
   return (
     <Fragment>
-        <a href="#!" onClick={e => props.handleCameraInfo(e, "Camera Info")} className="camera__info--icon">
+        <a title="See Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
           <li><i className="fas fa-info-circle"></i></li>
         </a>
     </Fragment>
