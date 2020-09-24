785f536d95659063b9b10270149a728286f8b705
Steven Polido <steven.polido@vistage.com>
2020-09-24 00:57:18 -0700

    Added local storage data to else in ternary

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,11 +2,12 @@ import React, { Fragment } from "react";
 import CameraInfo from './CameraInfo';
 
 const Photo = (props) => {
+  const cameraInfo = JSON.parse(localStorage.getItem('cameraInfoContent'));
   return (
     <Fragment>
         <section data-aos="fade" data-aos-delay="30">
           <CameraInfo handleRightSideBarClick={props.handleRightSideBarClick} currPhoto={props.currentPhoto} />
-            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : './img/bonitaskies.jpg' } />
+            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : cameraInfo.imagePath } />
         </section>
     </Fragment>
   );
