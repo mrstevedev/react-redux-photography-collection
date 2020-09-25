5473a1fb1e90bcc93c06fb0788eae0f36eca2333
Steven Polido <steven.polido@vistage.com>
2020-09-25 16:44:32 -0700

    Updated 404 not found component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const NotFound = () => (
-    <div>
-        The page you requested was not found.
-    </div>
+    <section className="not-found-page">
+        <h4>404</h4>
+        <p>The page you requested was not found.</p>
+    </section>
 );
 
 export default NotFound;
