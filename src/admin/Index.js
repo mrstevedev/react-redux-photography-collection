dfed2cd46b73d86b66e99c29c8e1804e994d6920
Steven Polido <steven.polido@vistage.com>
2020-09-29 13:40:30 -0700

    Added markup to error

--- src/admin/Index.js
+++ src/admin/Index.js
@@ -58,19 +58,19 @@ class Admin extends React.Component {
     render() {
         return (
             <div className="admin-container">
-                <p className="admin-signIn__error" style={{ color:' #ff9595', fontSize: '1rem', position: 'absolute',top: '23rem', 
-                    textTransform: 'none' }}>{ this.state.error ? this.state.error : '' }</p>
-                <h4 style={{ fontSize: '0.7rem' }}> Admin Sign In</h4>
+                <div className="admin-signIn__error-container" className={ this.state.error ? 'show' : 'hide' }>
+                <p>Error</p>
+                <p className="admin-signIn__error">{ this.state.error ? this.state.error : '' }</p>
+                </div>
+                <h4>Admin Sign In</h4>
                 <form className="admin-signIn__form">
                     <input type="text" autoFocus className={ `admin-signIn__input ${ this.state.errorUsername ? 'error' : '' }` } name="username" onChange={e => this.handleChange(e)} />
                     <input type="password" className={ `admin-signIn__input ${ this.state.errorPassword ? 'error' : '' }` } name="password" onChange={e => this.handleChange(e)} />
                     <button onClick={this.handleSignIn}>Sign-In</button>
                 </form>
 
-                <div className="admin-signIn__description" style={{ margin: '2rem 0', fontSize: '14px' }}>
-                    <p style={{ textTransform: 'none' }}>
-                        Backend developed with <u>Express</u>. User authentication <br/> with <u>Passport</u> and authorization with <u>JWT</u>.
-                    </p>
+                <div className="admin-signIn__description">
+                    <p>Backend developed with <u>Express</u>. User authentication <br/> with <u>Passport</u> and authorization with <u>JWT</u>.</p>
                 </div>
             </div>
         )
