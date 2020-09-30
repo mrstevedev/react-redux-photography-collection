4df237626a9e1ff8e482f9578d090cdf7ee0a6f8
Steven Polido <steven.polido@vistage.com>
2020-09-30 12:14:01 -0700

    Added logout onCLick handler

--- src/admin/components/Header.js
+++ src/admin/components/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <header>
-            header
+        <header style={{ display: 'flex',
+            justifyContent: 'flex-end',
+            padding: '1rem' }}>
+            <a href="#" onClick={props.handleLogout}>Logout</a>
         </header>
     )
 }
