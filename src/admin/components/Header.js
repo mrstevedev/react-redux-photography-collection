a3bd1136d0dc4c873c3d794f68abff17c3e169cd
Steven Polido <steven.polido@vistage.com>
2020-10-01 16:18:48 -0700

    Added child components passing props

--- src/admin/components/Header.js
+++ src/admin/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import AccountToggle from './AccountToggle';
+import AccountDropdown from './AccountDropdown';
 
 const Header = (props) => {
+    console.log('props in header', props)
     return (
         <header style={{ display: 'flex',
             justifyContent: 'flex-end',
-            padding: '1rem' }}>
-            <a href="#" onClick={props.handleLogout}>Logout</a>
+            padding: '1rem 2rem', height: '65px',
+            alignItems: 'center', boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.05)' }}>
+            <img src="../../public/img/logo.svg" className="logo" width="25" height="25" />
+            <div className="admin__dashboard-account">
+                <AccountToggle handleAccountToggle={props.handleAccountToggle} />
+                <AccountDropdown user={ props.user } handleLogout={props.handleLogout} />
+            </div>
         </header>
     )
 }
