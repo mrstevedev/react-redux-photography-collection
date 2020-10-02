d0ba4dd11f055412ec19ac4df7a7b0da1580667c
Steven Polido <steven.polido@vistage.com>
2020-10-02 13:54:18 -0700

    Added avatarIcon from prop passed in

--- src/admin/components/AccountToggle.js
+++ src/admin/components/AccountToggle.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-export default class AccountToggle extends Component {
-    constructor(props) {
-        super();
-    }
-    render() {
-        return (
-            <div
-            className="admin__dashboard-account-toggle" 
-            onClick={this.props.handleAccountToggle} style={{
-                width: '40px',
-                height: '40px',
-                borderRadius: '100%',
-                background: 'url("../public/img/rayrooftop.jpg") no-repeat center center',
-                backgroundSize: 'cover'                
-            }}>
-            </div>
-        )
-    }
+import React from "react";
+export default function AccountToggle(props) {
+  return (
+    <div
+      className="admin__dashboard-account-toggle"
+      onClick={props.handleAccountToggle}
+      style={{
+        width: "40px",
+        height: "40px",
+        borderRadius: "100%",
+        background: `url(${props.user.avatarIcon}) no-repeat center center`,
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
 }
