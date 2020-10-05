a9493544b8a1d3eacd2bf9253613c0d551e5c14b
Steven Polido <steven.polido@vistage.com>
2020-10-05 12:39:47 -0700

    Updated localStorage null check

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -7,7 +7,7 @@ const Photo = (props) => {
     <Fragment>
         <section data-aos="fade" data-aos-delay="30">
           <CameraInfo handleRightSideBarClick={props.handleRightSideBarClick} currPhoto={props.currentPhoto} />
-            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : cameraInfo.imagePath !== null ? cameraInfo.imagePath : './img/bonitaskies.jpg'} />
+            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : JSON.parse(localStorage.getItem('cameraInfoContent')) != null ? cameraInfo.imagePath : './img/bonitaskies.jpg'} />
         </section>
     </Fragment>
   );
