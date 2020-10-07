bd4b7d7aaf65b172272770cb447097829d9666b0
Steven Polido <steven.polido@vistage.com>
2020-10-07 13:24:14 -0700

    Commented out console.log statement

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 
 const CameraInfo = (props) => {
-  console.log('props:::::',props)
+  // console.log('props:::::',props)
   return (
     <Fragment>
         <a title="See Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
