ce2d300bc533c12a611c03f02184fd2bf7268b20
Steven Polido <steven.polido@vistage.com>
2020-10-07 12:27:08 -0700

    Added util/ folder with exported function

---
+++ util/authenticateToken.js
@@ -0,0 +1,8 @@
+module.exports = function authenticateToken(req, res, next) {
+    if (req.headers['requestapi']) {
+       // custom header exists, then call next() to pass to the next function
+       next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
