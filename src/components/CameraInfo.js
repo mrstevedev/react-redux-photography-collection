9f6b67d9daea2610bce56ad69d59b9147d9a74bf
Steven Polido <steven.polido@vistage.com>
2020-10-07 20:40:22 -0700

    Added tooltip attrinutes

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -4,8 +4,8 @@ const CameraInfo = (props) => {
   // console.log('props:::::',props)
   return (
     <Fragment>
-        <a title="See Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
-          <li><i className="fas fa-info-circle"></i></li>
+        <a flow="right" tooltip="Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
+          <i className="fas fa-info-circle"></i>
         </a>
     </Fragment>
   );
