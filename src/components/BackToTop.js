c2b5bd39ff143095c19bb7bb7de4d39c68d8559f
Steven Polido <steven.polido@vistage.com>
2020-10-09 11:45:21 -0700

    Replaced outer div with Fragment

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 
 const BackToTop = (props) => {  
     return (
-      <div>
+      <Fragment>
         <div className="back-to-top" data-aos="fade-up" data-aos-delay="1000" data-aos-offset="800">
           <a className="back-to-top-btn" onClick={topScrollTo}>
             <i className="fas fa-long-arrow-alt-up"></i>Back To Top
           </a>
         </div>
-      </div>
+      </Fragment>
     );
 }
 
