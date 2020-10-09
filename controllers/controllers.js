86effdb4c6c1aff4f92d96175972dcb3d81ba1c5
Steven Polido <steven.polido@vistage.com>
2020-10-08 18:28:16 -0700

    Commented out external json file and response.json() response

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,8 @@
+// const api = require("../api/photos.json");
 const Photo = require("../models/Photo");
 
 const getAPI = (req, res, next) => {
+  //   res.status(200).json(api);
   Photo.find()
     .then((photos) => {
       res.status(200).json(photos);
