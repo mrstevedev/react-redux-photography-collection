bc5864aa38519b35706f173410132c6b34e44d0d
Steven Polido <steven.polido@vistage.com>
2020-10-22 19:11:12 -0700

    Cleaned up store

--- src/store/index.js
+++ src/store/index.js
@@ -2,28 +2,6 @@
 import { createStore } from "redux";
 import reducer from "../reducers";
 
-// Create the initial state
-const initialState = {
-  imageName: "bonitaskies.jpg",
-  imagePath: "./img/bonitaskies.jpg",
-};
-// const initialState = {
-//   photos: [
-//     {
-//       photo_1: {}
-//     },
-//     {
-//       photo_2: {}
-//     },
-//     {
-//       photo_3: {}
-//     },
-//     {
-//       photo_4: {}
-//     }
-//   ]
-// };
-
 export const store = createStore(
   reducer,
   initialState,
