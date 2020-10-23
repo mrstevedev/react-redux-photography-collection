c0dcb58853b26eba11c3577e0db274690cb3859b
Steven Polido <steven.polido@vistage.com>
2020-10-22 19:13:28 -0700

    Removed initialState from createStore

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,5 @@ import reducer from "../reducers";
 
 export const store = createStore(
   reducer,
-  initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
