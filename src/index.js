a085419ed8d2c50dede3be2c1965777972a380a8
Steven Polido <steven.polido@vistage.com>
2020-10-24 16:17:32 -0700

    Removed store out of named import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./App.scss";
-import { store } from "./store";
+import store from "./store";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
