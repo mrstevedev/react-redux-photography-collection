ab18ed6dd17d0dcc422339348c5b962da848b802
Steven Polido <steven.polido@vistage.com>
2020-10-24 16:25:54 -0700

    Added async action to fetch from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,38 @@
-import { SET_PHOTO } from '../constants/actionTypes';
+import * as types from "../constants/actionTypes";
+import axios from "axios";
+const { REACT_APP_API_URL } = process.env;
+
+export const getItemsAction = () => ({
+  return(dispatch) {
+    axios.get(`${REACT_APP_API_URL}/photos`).then((response) => {
+      console.log(response);
+      dispatch({ type: types.LOAD_PHOTOS_SUCCESS, payload: response.data });
+    });
+  },
+});
 
 export function setPhoto(e, id, val) {
-    return {
-        type: SET_PHOTO,
-        id: id,
-        href: val
-    }
+  return {
+    type: SET_PHOTO,
+    id: id,
+    href: val,
+  };
 }
+
+// Async action
+export const fetchPhotos = () => {
+  return (dispatch) => {
+    axios
+      .get(`${REACT_APP_API_URL}/photos`, {
+        headers: {
+          requestapi: "123",
+        },
+      })
+      .then((res) => {
+        dispatch({
+          type: types.LOAD_PHOTOS_SUCCESS,
+          payload: res.data,
+        });
+      });
+  };
+};
