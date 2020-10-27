e1fa246a66c99a841022c2a3fc7140e66b8478ce
Steven Polido <steven.polido@vistage.com>
2020-10-27 14:31:51 -0700

    Added AOS fade-in to info icon

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -4,7 +4,7 @@ const CameraInfo = (props) => {
   // console.log('props:::::',props)
   return (
     <Fragment>
-        <a flow="right" tooltip="Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
+        <a  data-aos="fade-in" data-aos-delay="1500" flow="right" tooltip="Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
           <i className="fas fa-info-circle"></i>
         </a>
     </Fragment>
