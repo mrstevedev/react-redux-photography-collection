72d416f632be8a74607721141817a941a6d1f1ce
Steven Polido <steven.polido@vistage.com>
2020-10-27 14:02:34 -0700

    Replaced image with state from store

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import CameraInfo from './CameraInfo';
+import { useSelector } from 'react-redux';
 
 const Photo = (props) => {
+  const photo = useSelector((state) => state.currentPhoto);
   const cameraInfo = JSON.parse(localStorage.getItem('cameraInfoContent'));
   return (
     <Fragment>
         <section data-aos="fade" data-aos-delay="30">
           <CameraInfo handleRightSideBarClick={props.handleRightSideBarClick} currPhoto={props.currentPhoto} />
-            <img src={ props.currentPhoto.imagePath ? props.currentPhoto.imagePath : JSON.parse(localStorage.getItem('cameraInfoContent')) != null ? cameraInfo.imagePath : './img/bonitaskies.jpg'} />
+            <img src={ photo.currentPhoto.imagePath ? photo.currentPhoto.imagePath : JSON.parse(localStorage.getItem('cameraInfoContent')) != null ? cameraInfo.imagePath : './img/bonitaskies.jpg'} />
         </section>
     </Fragment>
   );
