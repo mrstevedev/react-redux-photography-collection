d38a15e3e5c72bcb6661bf2ee4a9277ec993c58f
Steven Polido <steven.polido@vistage.com>
2020-10-26 18:31:44 -0700

    Renamed reducer allReducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 // Create Redux Store
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import reducer from "../reducers";
+import allReducers from "../reducers";
 
 export default createStore(
-  reducer,
+  allReducers,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && 
       window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
