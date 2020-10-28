cc3c23553e791f52d5f57da90e4a2a6b4202e555
Steven Polido <steven.polido@vistage.com>
2020-10-28 11:21:00 -0700

    Added dispatch to showSidebar action creator

--- src/components/CameraInfo.js
+++ src/components/CameraInfo.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from "react";
+import { useDispatch } from 'react-redux';
+import { showSidebar } from '../actions';
 
 const CameraInfo = (props) => {
-  // console.log('props:::::',props)
+  const dispatch = useDispatch();
   return (
     <Fragment>
-        <a  data-aos="fade-in" data-aos-delay="1500" flow="right" tooltip="Current Photo Info" href="#!" onClick={e => props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")} className="camera__info--icon">
-          <i className="fas fa-info-circle"></i>
-        </a>
+      <a
+        data-aos="fade-in"
+        data-aos-delay="1500"
+        flow="right"
+        tooltip="Current Photo Info"
+        href="#!"
+        onClick={(e) => dispatch(showSidebar(e, 'Camera Info'))}
+        // onClick={(e) =>
+        //   props.handleRightSideBarClick(e, props.currPhoto, "Camera Info")
+        // }
+        className="camera__info--icon"
+      >
+        <i className="fas fa-info-circle"></i>
+      </a>
     </Fragment>
   );
 };
