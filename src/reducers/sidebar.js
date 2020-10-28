bd614fd7b3ab1f9ee045d615f172bc63e4c889a5
Steven Polido <steven.polido@vistage.com>
2020-10-28 12:24:49 -0700

    Added new case HIDE_SIDEBAR

--- src/reducers/sidebar.js
+++ src/reducers/sidebar.js
@@ -4,6 +4,8 @@ export default function sidebarReducer(state = false, action) {
     switch(action.type) {
         case types.SHOW_SIDEBAR:
             return state = true
+        case types.HIDE_SIDEBAR:
+            return state = false
         default:
             return state
     }
