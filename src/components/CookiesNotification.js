23a1ad5d0303123a0e730fd18d15112ffff65172
Steven Polido <steven.polido@vistage.com>
2020-10-29 12:00:03 -0700

    Added action, useDispatch hook and refactored events to dispatch action

--- src/components/CookiesNotification.js
+++ src/components/CookiesNotification.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { closeNotification } from '../actions';
+import { useDispatch } from 'react-redux';
 
 export default function CookiesNotification(props) {
+    const dispatch = useDispatch();
     return (
         <div data-aos="fade-in" data-aos-delay="30" className="cookies__notification">
-            <header><i className="fas fa-times close" onClick={props.handleCloseCookieModal}></i></header>
+            <header><i className="fas fa-times close" onClick={() => dispatch(closeNotification()) }></i></header>
             <p>This site uses cookies to provide a great user experience. <br /> By using this site, you agree to our use of cookies.</p>
-            <button className="cookies__notification-acceptBtn" onClick={props.handleCloseCookieModal}>Accept</button>
+            <button className="cookies__notification-acceptBtn" onClick={() => dispatch(closeNotification())}>Accept</button>
             <button className="cookies__notification-moreBtn" href="/about-cookies">Read More</button>
         </div>
     );
