0d3a446bdf2fbfec6185daf23803793c371d2792
Steven Polido <steven.polido@vistage.com>
2020-10-29 12:00:56 -0700

    Added notification reducer

--- src/reducers/notification.js
+++ src/reducers/notification.js
@@ -1,9 +1,11 @@
 import * as types from '../constants/actionTypes';
 
-export default function notificationReducer(state = false, action) {
+export default function notificationReducer(state = 'hide', action) {
     switch(action.type) {
         case types.CLOSE_NOTIFICATION:
-            return state = true
+            return state = 'hide'
+        case types.SHOW_NOTIFICATION:
+            return state = 'show'
         default:
             return state
     }
